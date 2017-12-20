1b4b68dae6f80c44e0b96073d80111e0c0fd0eeb
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2017-12-20 15:09:26 +0900

    Userテーブル作成

--- roamers/db/schema.rb
+++ roamers/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219162254) do
+ActiveRecord::Schema.define(version: 20171220060802) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
