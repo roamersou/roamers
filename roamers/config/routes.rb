f6091ca9fc74619c113894d5e6e1c8fe2a3d7060
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2017-12-21 11:37:47 +0900

    プロフィールページ作成

--- roamers/config/routes.rb
+++ roamers/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+
+  
+  get "posts/index" => "posts#index"
 
   get "/" => "home#top"
   get "about" => "home#about"
-  
-  get "posts/index" => "posts#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
