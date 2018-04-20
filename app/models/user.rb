2164b8b708382f90b64e629755422e7881b1aa99
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-04-20 17:15:24 +0900

    永続セッションのトークン作成と暗号化

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_digest :string(255)
 #
 # Indexes
 #
@@ -15,6 +16,7 @@
 #
 
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { self.email = email.downcase }
     validates :name, presence: true, length:{maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -30,5 +32,16 @@ class User < ApplicationRecord
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                 BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
+    end
+
+    # ランダムなトークンを返す
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    # 永続セッションのためにユーザーをデータベースに記憶する
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
     end
 end
