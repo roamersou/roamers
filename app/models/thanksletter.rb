8f00703282e4f760dce7e7b2a4ed7ee6a5abb534
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-11 12:32:20 +0900

    サンクスレターをユーザーへ従属させる

--- app/models/thanksletter.rb
+++ app/models/thanksletter.rb
@@ -20,6 +20,7 @@
 
 class Thanksletter < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 500}
 end
