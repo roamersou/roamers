69f04aecc5bdd89f6e330673a20924a09200176b
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-18 02:58:19 +0900

    サンクスレターの削除機能追加

--- app/controllers/thanksletters_controller.rb
+++ app/controllers/thanksletters_controller.rb
@@ -1,5 +1,6 @@
 class ThankslettersController < ApplicationController
     before_action :logged_in_user, only: [:new, :create, :destroy]
+    before_action :correct_user, only: :destroy
 
     def new
         @user_id = params[:user_id]
@@ -17,11 +18,19 @@ class ThankslettersController < ApplicationController
     end
 
     def destroy
+        @thanksletter.destroy
+        flash[:success] = "サンクスレターを削除しました"
+        redirect_to request.referrer || users_url
     end
 
     private
 
     def thanksletter_params
         params.require(:thanksletter).permit(:content)
+    end
+
+    def correct_user
+        @thanksletter = current_user.thanksletters.find_by(id: params[:id])
+        redirect_to users_url if @thanksletter.nil?
     end
 end
