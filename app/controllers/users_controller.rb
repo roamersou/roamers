ee875dc12e4849cc4a1ed09b1c3d90e2a356af99
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-21 17:46:34 +0900

    yarn_integrityのfalseとフォームにアップロード追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :self_intro, :fb_account, :tw_account, :insta_account)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :self_intro, :fb_account, :tw_account, :insta_account, :picture)
   end
 
   #beforeフィルター
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@
 #  fb_account      :string(255)
 #  insta_account   :string(255)
 #  admin           :boolean          default(FALSE)
+#  picture         :string(255)
 #
 # Indexes
 #
@@ -24,6 +25,7 @@ class User < ApplicationRecord
     has_many :thanksletters, dependent: :destroy
     attr_accessor :remember_token
     before_save { self.email = email.downcase }
+    mount_uploader :picture, PictureUploader
     validates :name, presence: true, length:{maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length:{maximum: 255},
